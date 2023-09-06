f8dae54fb13b8462076e4f313072dfb8fb4562c9
travis544 <traviszheng544@gmail.com>
2023-09-06 12:30:23 -0700

    removed node modules

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,18 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
